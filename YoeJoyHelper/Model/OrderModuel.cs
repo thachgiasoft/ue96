6754509d8d7d94ea5d1919a1a86e7e01b38ee438
leeyon <missile2006@126.com>
2013-01-24 14:22:52 +0800

    add order module remove entityframework
    Signed-off-by: leeyon <missile2006@126.com>

---
+++ YoeJoyHelper/Model/OrderModuel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Icson.Objects.Basic;
+using Icson.Objects.Online;
+using Icson.Objects;
+using Icson.Objects.Finance;
+using Icson.Utils;
+using Icson.BLL;
+using Icson.BLL.Online;
+using Icson.BLL.Sale;
+using Icson.BLL.Finance;
+using Icson.BLL.RMA;
+using System.Data;
+using System.Data.Sql;
+
+namespace YoeJoyHelper.Model
+{
+    public class OrderModuel
+    {
+        public string SysNo { get; set; }
+
+    }
+}
